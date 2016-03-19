dfd2360056932fdfb709bc1956afcd52e094a414
pichon_e <pichon_e@epitech.eu>
2016-03-20 00:48:24 +0100

    dialog choix medecin

--- src/app/chat/chat.controller.js
+++ src/app/chat/chat.controller.js
@@ -127,7 +127,6 @@
       ];
 
       $mdDialog.show({
-        controller: ChatController,
         templateUrl: 'app/chat/medecinChoice.tmpl.html',
         parent: angular.element(document.body),
         targetEvent: ev,
