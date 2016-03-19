06c2057521fed226f543586ff95aa2ed7e49fe7a
Oprax <oprax@me.com>
2016-03-19 23:44:45 +0100

    PDF reader

--- src/app/chat/chat.controller.js
+++ src/app/chat/chat.controller.js
@@ -6,7 +6,42 @@
     .controller('ChatController', ChatController);
 
   /** @ngInject */
-  function ChatController($scope) {
+  function ChatController($scope, $mdDialog, $mdMedia) {
+    $scope.showPdf = function (ev) {
+    	$mdDialog.show({
+    		controller: PdfController,
+    		templateUrl: "app/chat/pdfDialog.tmpl.html",
+    		parent: angular.element(document.body),
+    		targetEvent: ev,
+    		clickOutsideToClose: true,
+    		fullscreen: true
+    	})
+    	.then(function(answer) {
+    		console.log(answer);
+    	}, function() {
+    		console.log('Nope !');
+    	});
+    };
+  }
 
+  function PdfController($scope, pdfDelegate, $mdDialog) {
+  	$scope.pdfUrl = 'app/chat/ordonnance.pdf';
+  	$scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+    var pdfHandle = pdfDelegate.$getByHandle('pdfView');
+
+    $scope.zoomIn = function(ev) {
+    	pdfHandle.zoomIn();
+    };
+    $scope.zoomOut = function(ev) {
+    	pdfHandle.zoomOut();
+    };
+    $scope.prev = function(ev) {
+    	pdfHandle.prev();
+    };
+    $scope.next = function(ev) {
+    	pdfHandle.next();
+    };
   }
 })();
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('slack4healthHhc', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('slack4healthHhc', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'pdf']);
 
 })();
