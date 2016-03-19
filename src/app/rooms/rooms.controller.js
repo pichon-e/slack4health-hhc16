2dce66569110716943ef9e117d5e2d02d730e2f5
Gab-Metzger <gab.metzger@gmail.com>
2016-03-19 13:38:12 +0100

    Init rooms controller

---
+++ src/app/rooms/rooms.controller.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('slack4healthHhc')
+    .controller('RoomsController', RoomsController);
+
+  /** @ngInject */
+  function RoomsController($scope) {
+    $scope.rooms = [
+    {
+        name: "Chambre 1",
+        patient: {
+            first_name: "Bob",
+            last_name:"Bobi"
+        }
+    },
+    {
+        name: "Chambre 2"
+    }]
+  }
+})();
