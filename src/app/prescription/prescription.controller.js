1c3dcfef492b68b9ed6355cc2ebc91df015ed71b
Oprax <oprax@me.com>
2016-03-19 16:12:29 +0100

    Add medicaments list in prescription controller

--- src/app/prescription/prescription.controller.js
+++ src/app/prescription/prescription.controller.js
@@ -7,6 +7,34 @@
 
   /** @ngInject */
   function PrescriptionController($scope) {
+    $scope.medicaments = {
+      "medicaments": [
+        {
+          "name": "Diamicron",
+          "dosage": "30mg",
+          "forme": "comprimé",
+          "posologie": "3-0-0"
+        },
+        {
+          "name": "Januvia",
+          "dosage": "100mg",
+          "forme": "comprimé",
+          "posologie": "1-0-0"
+        },
+        {
+          "name": "Metformine",
+          "dosage": "1000mg",
+          "forme": "comprimé",
+          "posologie": "1-1-1"
+        },
+        {
+          "name": "Tiatec",
+          "dosage": "2.5mg",
+          "forme": "comprimé",
+          "posologie": "1-0-0"
+        }
+      ]
+    };  
 
   }
 })();
