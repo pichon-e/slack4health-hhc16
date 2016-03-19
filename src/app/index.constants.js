5a6c41b65adf0345b02d5ff4ccd5fb30c8035d7f
Gab-Metzger <gab.metzger@gmail.com>
2016-03-19 13:14:51 +0100

    Init project with yeoman gulp angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('slack4healthHhc')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('slack4healthHhc', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
